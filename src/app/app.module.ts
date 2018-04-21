1e60cf2d507d745053381560379eeb3f4519182c
prashantsulkshane14 <prashantsulkshane@gmail.com>
2018-04-22 01:08:43 +0530

    Template driven form with some basic validation  added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms'
 
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { AngularMaterialModule } from './angular-material.module';
+import { RegistrationComponent } from './component/registration/registration.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
